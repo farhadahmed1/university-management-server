38a58e0b7afadab71aaca2e04f86b68653722e23
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2024-05-18 10:15:09 +0600

    added mongoos validator

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -7,9 +7,30 @@ import {
 } from './student/student.interface';
 
 const userNameSchema = new Schema<UserName>({
-  firstName: { type: String, required: true },
-  middleName: String,
-  lastName: { type: String, required: true },
+  firstName: {
+    type: String,
+    trim: true,
+    required: [true, 'First Name is required'],
+    maxLength: [20, 'First name can not exceed 20 characters'],
+
+    // validation functions
+    validate: {
+      validator: function (value: string) {
+        const capitalizedStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return capitalizedStr === value;
+      },
+      message: '{VALUE} is not in capitalized format',
+    },
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, 'Last Name is required'],
+  },
 });
 
 const guardianSchema = new Schema<Guardian>({
@@ -22,7 +43,7 @@ const guardianSchema = new Schema<Guardian>({
 });
 
 const localGuardianSchema = new Schema<LocalGuardian>({
-  name: { type: String, required: true },
+  name: { type: String, trim: true, required: true },
   occupation: String,
   contactNo: String,
   address: { type: String, required: true },
@@ -30,11 +51,30 @@ const localGuardianSchema = new Schema<LocalGuardian>({
 
 const studentSchema = new Schema<Student>({
   id: { type: String, required: true, unique: true }, // Assuming 'id' is a student ID
-  name: userNameSchema,
-  gender: { type: String, enum: ['male', 'female'] },
+  name: {
+    type: userNameSchema,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female', 'other'],
+      message:
+        "The gender field can only be one of the following :'male','female', or :'other'.",
+    },
+  },
   dateOfBirth: Date,
-  email: { type: String, required: true, unique: true },
-  contactNo: String,
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  contactNo: {
+    type: String,
+    required: [true, 'Contact number is required'],
+  },
   emergencyContact: String,
   bloodGroup: {
     type: String,
@@ -42,8 +82,14 @@ const studentSchema = new Schema<Student>({
   },
   permanentAddress: String,
   presentAddress: { type: String, required: true },
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
+  guardian: {
+    type: guardianSchema,
+    required: true,
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: true,
+  },
   profileImg: String,
   isActive: { type: String, enum: ['active', 'disabled'], default: 'active' },
 });
