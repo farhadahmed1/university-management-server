ec96b990f66caa1c084a097a35d2510b3ebaf8ef
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2024-05-27 01:23:08 +0600

    added user route refeactors

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,7 +4,18 @@ import { StudentRoutes } from '../modules/student/student.route';
 
 const router = Router();
 
-router.use('/users', UserRoutes);
-router.use('/students', StudentRoutes);
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
+  },
+];
 
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
 export default router;
