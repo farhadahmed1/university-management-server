00756f46c95038578dc8ef58e100bb4d81701545
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2024-05-29 11:24:08 +0600

    added user id validation

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -17,13 +17,27 @@ const findLastStudentId = async () => {
     .lean();
 
   //203001   0001
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generatedStudentId = async (payload: TAcademicSemester) => {
   // first time 0000
   //0001  => 1
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId?.substring(6); // 0001
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
