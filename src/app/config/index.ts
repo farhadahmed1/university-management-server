d8ff3bcc8e776d7cd9e9c754289d337c76a5f5a4
farhadahmed1 <farhad.ahmed.aust@gmail.com>
2024-09-22 21:06:34 +0600

    added forget password

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,4 +10,5 @@ export default {
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
   jwt_access_secret: process.env.ACCESS_JWT_SECRET,
+  Jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
 };
